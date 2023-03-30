7063793f82c5791383ac01ecbbfe4f5c33c3eb13
@aamutezar <aamutezar@gmail.com>
2023-03-30 14:15:11 +0700

    update slicing  success checkout

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/login', function () {
 Route::get('/checkout', function () {
     return view('checkout');
 })->name('checkout');
+
+Route::get('/checkout-success', function () {
+    return view('success_checkout');
+})->name('success_checkout');
